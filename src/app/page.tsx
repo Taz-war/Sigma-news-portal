08c1590e24ae952fe78d638a02dcf4322ebb861e
Taz-war <fahimtazwer@gmail.com>
2024-03-09 23:02:28 +0600

    new features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
 
 export default function Home() {
   return (
     <div className=" p-2">
       {/* <h1>Welcome to homePage !!!</h1> */}
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="min-h-[200px]  rounded-lg border"
+      >
+        <ResizablePanel defaultSize={75}>
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold">Content</span>
+          </div>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={25}>
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold">Sidebar</span>
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 }
